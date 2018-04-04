4340489fd984c5b9199b1a5652318e5e8dc5f696
jonibeatles <jonibeatles@gmail.com>
2018-04-04 14:39:22 -0300

    comentarios

--- nice_tree/nice.h
+++ nice_tree/nice.h
@@ -1,4 +1,4 @@
-//Unica funçao a ser usada é o getNiceTree para pegar a nice tree
+//As unicas funçoes que devem ser chamas estao com comentario indicando. O resto é tudo funçao interna.
 #ifndef H_NICE_
 #define H_NICE_
 #include <bits/stdc++.h>
@@ -17,8 +17,11 @@ struct Nice {
 
   bool isNiceAux(int vertex, int pai,vector<int> bagAnterior,int join);
 
+  //chame esta funçao para verificar se a arvore tem as propriedades nice, deve ser chamada em conjunto com a isTD para verificar se é nice TD.
+  //adicionalmente a funçao verifica se as bags estao ordenadas, lembrando que em diversos pontos do programa assumimos as bags estarem ordenadas.
   bool isNice(vector<vector <int> > tree1,vector<vector <int> > bags1, vector<bool> isTerminal1, int root1);
 
+  //esta funcao verifica se é uma tree decomposition.
   bool isTD(vector<vector <int> > tree1,vector<vector <int> > bags1, vector<bool> isTerminal1, int root1, vector<vector <pair <int,int>>> graph);
 
   void Debug();
@@ -37,8 +40,7 @@ struct Nice {
 
   int findTerminal(vector<int> bag,vector<bool> isTerminal);
 
-  //unica funcao que deve ser chamada é essa, para pegar a nice.
-  //retorna uma struct niceTW com a arvore e as bags, elementos das bags ordenados, raiz garantidamente tem um terminal
+  //retorna uma struct niceTW com a nice tree. os elementos das bags estao ordenados e a raiz garantidamente tem um terminal.
   niceTW getNiceTree(vector<vector <int> > tree1,vector<vector <int> > bags1, vector<bool> isTerminal1,int tam_bag1);
 
   void chooseRoot();
@@ -48,9 +50,9 @@ struct Nice {
   int bag_count; //numero de bags (vertices) da nice tree decomposition
   int root;	//raiz da tree decomposition
   int tam_bag;	//tamanho maximo de uma bag (tw + 1)
-  vector<bool> isTerminal;
-  vector<bool> visited;
-  vector<vector<int>> caminhos;
+  vector<bool> isTerminal; //booleanos indicando se eh terminal
+  vector<bool> visited; //auxiliar para o dfs
+  vector<vector<int>> caminhos; //auxiliar para o isTD
 };
 
 #endif
